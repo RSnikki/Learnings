6c4b5084b63b3402ecaad02a81c451cba328cabe
Nikhil Ranjan Singh <nikhilranjansingh16@gmail.com>
2022-12-14 23:05:43 +0530

    Ques9

---
+++ Ques9/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques9
+{
+    class Program
+    {
+        public static bool findArmstrongNo(int num)
+        {
+            int dig, dig1, sum = 0, numb =num, num1= num, count = 0;
+
+            while (num1 > 0)
+            {
+                dig1 = num1 % 10;
+                count = count + 1;
+                num1 = num1 / 10;
+            }
+
+            while (numb > 0)
+            {
+                dig = numb % 10;
+                sum = sum + (dig ^ count);
+                numb = numb / 10;
+            }
+            
+            if (sum == num)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            bool result = findArmstrongNo(n);
+
+            if (result == true)
+            {
+                Console.WriteLine("{0} is a Armstrong no.", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Armstrong no.", n);
+            }
+
+        }
+    }
+}
