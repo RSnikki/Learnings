8d553d28360e5ceff649262991f14c91dcb57768
Nikhil Ranjan Singh <nikhilranjansingh16@gmail.com>
2022-12-16 14:26:39 +0530

    Abstract Prog1

---
+++ AbstractDemo/Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDemo
+{
+    /*Abstract modifier can be used with classes, properties, methods, indexers and events.*/
+    public abstract class AbstractClass
+    {
+        public abstract void Print(String name);
+    }
+
+    /*Abstract class's Abstract method if not implemented in derived class will produce compile error*/
+    public class Derived : AbstractClass
+    {
+        public override void Print(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            /*Abstract Class cannot be instantiated. To access its members we derive it in a derived class 
+             and then create objects of derived class to access its members*/
+            Derived d1 = new Derived();
+            d1.Print("Welcome to 366Pi");
+            Console.ReadLine();
+        }
+    }
+}
