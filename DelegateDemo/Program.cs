76529ff27d1c897427366041e909934c369e6e48
Nikhil Ranjan Singh <nikhilranjansingh16@gmail.com>
2022-12-30 00:57:26 +0530

    properly spaced

--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -6,68 +6,71 @@ using System.Threading.Tasks;
 
 namespace DelegateDemo
 {
-    
-        public delegate int MyDelegate(int num1, int num2);
+    public delegate int MyDelegate(int num1, int num2);
 
-        public class Example
+    public class Example
+        
+    {
+            
+        // methods to be assigned and called by delegate
+        public int Sum(int num1, int num2)
         {
-            // methods to be assigned and called by delegate
-            public int Sum(int num1, int num2)
-            {
-                return num1 + num2;
-            }
-
-            public int Difference(int num1, int num2)
-            {
-                return num1 - num2;
-            }
-
-            public int Product(int num1, int num2)
-            {
-                return num1 * num2;
-            }
-
-            public int Modulo(int num1, int num2)
-            {
-                return num1 % num2;
-            }
-
-            public int Quotient(int num1, int num2)
-            {
-                return num1 / num2;
-            }
+           return num1 + num2;
         }
 
-        class Program
+            
+        public int Difference(int num1, int num2)
         {
-            static void Main(string[] args)
-            {
-                Example obj = new Example();
+           return num1 - num2;
+        }
 
-                // Assigning the method using a constructor of the delegate
-                MyDelegate sum = new MyDelegate(obj.Sum);
-                // Assigning the method without the constructor of the delegate
-                MyDelegate diff = obj.Difference;
+            
+        public int Product(int num1, int num2)
+        {
+           return num1 * num2;
+        }
 
-                // Using delegate object to invoke the method(s)
-                Console.WriteLine("Sum of two integer is = " + sum(10, 20));
-                // Using Invoke() to invoke the method(s)
-                Console.WriteLine("Difference of two integer is = " + diff.Invoke(20, 10));
+        public int Modulo(int num1, int num2)
+        {
+           return num1 % num2;
+        }
 
-                // 3. More than one method can be invoked at once using multicast delegates
-                Console.WriteLine("\n----- Multicast delegate operations -----");
-                MyDelegate allOperations = obj.Sum;
-                allOperations += obj.Difference;
-                allOperations += obj.Product;
-                allOperations += obj.Modulo;
-                allOperations += obj.Quotient;
-                foreach (MyDelegate delOperation in allOperations.GetInvocationList())
-                {
-                    Console.WriteLine("Result: " + delOperation(20, 10));
-                }
+        public int Quotient(int num1, int num2)
+        {
+           return num1 / num2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj = new Example();
+
+            // Assigning the method using a constructor of the delegate
+            MyDelegate sum = new MyDelegate(obj.Sum);
+            // Assigning the method without the constructor of the delegate
+            MyDelegate diff = obj.Difference;
+
+            // Using delegate object to invoke the method(s)
+            Console.WriteLine("Sum of two integer is = " + sum(10, 20));
+            // Using Invoke() to invoke the method(s)
+            Console.WriteLine("Difference of two integer is = " + diff.Invoke(20, 10));
+
+            //More than one method can be invoked at once using multicast delegates
+            Console.WriteLine("\n----- Multicast delegate operations -----");
+            MyDelegate allOperations = obj.Sum;
+            allOperations += obj.Difference;
+            allOperations += obj.Product;
+            allOperations += obj.Modulo;
+            allOperations += obj.Quotient;
                 
+            foreach (MyDelegate delOperation in allOperations.GetInvocationList())
+            {
+               Console.WriteLine("Result: " + delOperation(20, 10));
             }
+            Console.ReadLine();
+                
         }
-    
-
+    }
 }
