4876ae27a5d815ba7f58e7444703f63d296651ec
Nikhil Ranjan Singh <nikhilranjansingh16@gmail.com>
2023-02-12 16:04:59 +0530

    Demo 1st Push

---
+++ DemoProjectAsp/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using DemoProjectAsp.Models;
+using DemoProjectAsp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProjectAsp.Controllers
+{
+   // [Route("[controller]/[action]/{id?}")]
+    
+    public class HomeController : Controller
+    {
+        //List<Book> _book;
+        IRepository<Book> _repo;
+        public HomeController(IRepository<Book> rbook)
+        {
+            //_book = new List<Book>();
+            _repo =  rbook;
+           
+        }
+
+       
+        public IActionResult Index()
+        {
+            return View(_repo.GetAll());
+        }
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                Book item = new Book()
+                {
+                    
+                    Title = book.Title,
+                    Author = book.Author,
+                    Description = book.Description,
+                    PublishedDate = book.PublishedDate,
+                    Price = book.Price
+                };
+                _repo.Add(item);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoProjectAsp/Services/SqlBookRepository.cs
@@ -0,0 +1,59 @@
+﻿using DemoProjectAsp.DataLayer;
+using DemoProjectAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProjectAsp.Services
+{
+    public class SqlBookRepository : IRepository<Book>
+    {
+        BookStoreDbContext _context;
+        public SqlBookRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Book item)
+        {
+            try
+            {
+                _context.Add(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Get(int id)
+        {
+            if (_context.Book.Count(x => x.Id == id) > 0)
+            {
+                return _context.Book.FirstOrDefault(x => x.Id == id);
+            }
+            else
+                return null;
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+           return _context.Book;
+        }
+
+
+    }
+}
